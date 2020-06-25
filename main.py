8d7d51ac5b9bff705ebcd3383ca03f353f199bd7
Jianfeng Zhang <swordfeng123@gmail.com>
2020-06-24 17:41:24 -0700

    sample main file

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 #!/usr/bin/python3
 
-import timelapse
+from timelapse import *
 
-print(dir(timelapse))
+webhook = YoutubeWebhook(
+    ('127.0.0.1', 18001),
+    'https://<webhook-url>',
+)
+
+YoutubeChannelWatcher('UCp6993wxpyDPHUpavwDFqgg', 'videos/sora', webhook=webhook)
+YoutubeChannelWatcher('UCDqI2jOz0weumE8s7paEk6g', 'videos/roboco', webhook=webhook)
+YoutubeChannelWatcher('UC-hM6YJuNYVAmUWxeIr9FeA', 'videos/miko', webhook=webhook)
+YoutubeChannelWatcher('UC5CwaMl1eIgY8h02uZw7u8A', 'videos/suisei', webhook=webhook)
+
+YoutubeChannelWatcher('UC0TXe_LYZ4scaW2XMyi5_kw', 'videos/azki', webhook=webhook)
+
+YoutubeChannelWatcher('UCD8HOxPs4Xvsm8H0ZxXGiBw', 'videos/mel', webhook=webhook)
+YoutubeChannelWatcher('UCQ0UDLQCjY0rmuxCDE38FGg', 'videos/matsuri', webhook=webhook)
+YoutubeChannelWatcher('UC1CfXB_kRs3C-zaeTG3oGyg', 'videos/haato', webhook=webhook)
+YoutubeChannelWatcher('UCHj_mh57PVMXhAUDphUQDFA', 'videos/haato', webhook=webhook)
+YoutubeChannelWatcher('UCFTLzh12_nrtzqBPsTCqenA', 'videos/akirose', webhook=webhook)
+YoutubeChannelWatcher('UCLbtM3JZfRTg8v2KGag-RMw', 'videos/akirose', webhook=webhook)
+YoutubeChannelWatcher('UCdn5BQ06XqgXoAxIhbqw5Rg', 'videos/fubuki', webhook=webhook)
+
+YoutubeChannelWatcher('UC1opHUrw8rvnsadT-iGp7Cg', 'videos/aqua', webhook=webhook)
+YoutubeChannelWatcher('UCXTpFs_3PqI41qX2d9tL2Rw', 'videos/shion', webhook=webhook)
+YoutubeChannelWatcher('UC7fk0CB07ly8oSl0aqKkqFg', 'videos/ayame', webhook=webhook)
+YoutubeChannelWatcher('UC1suqwovbL1kzsoaZgFZLKg', 'videos/choco', webhook=webhook)
+YoutubeChannelWatcher('UCp3tgHXw_HI0QMk1K8qh3gQ', 'videos/choco', webhook=webhook)
+YoutubeChannelWatcher('UCvzGlP9oQwU--Y0r9id_jnA', 'videos/subaru', webhook=webhook)
+
+YoutubeChannelWatcher('UCp-5t9SrOQwXMU7iIjQfARg', 'videos/mio', webhook=webhook)
+YoutubeChannelWatcher('UCvaTdHTWBGv3MKj3KVqJVCw', 'videos/okayu', webhook=webhook)
+YoutubeChannelWatcher('UChAnqc_AY5_I3Px5dig3X1Q', 'videos/korone', webhook=webhook)
+
+YoutubeChannelWatcher('UC1DCedRgGHBdm81E1llLhOQ', 'videos/pekora', webhook=webhook)
+YoutubeChannelWatcher('UCl_gCybOJRIgOXw6Qb4qJzQ', 'videos/rushia', webhook=webhook)
+YoutubeChannelWatcher('UCvInZx9h3jC2JzsIzoOebWg', 'videos/flare', webhook=webhook)
+YoutubeChannelWatcher('UCdyqAaZDKHXg4Ahi7VENThQ', 'videos/noel', webhook=webhook)
+YoutubeChannelWatcher('UCCzUftO8KOVkV4wQG1vkUvg', 'videos/marine', webhook=webhook)
+
+YoutubeChannelWatcher('UCZlDXzGoo7d44bwdNObFacg', 'videos/kanata', webhook=webhook)
+YoutubeChannelWatcher('UCS9uQI-jC3DE0L4IpXyvr6w', 'videos/coco', webhook=webhook)
+YoutubeChannelWatcher('UCqm3BQLlJfvkTsX_hvm0UmA', 'videos/watame', webhook=webhook)
+YoutubeChannelWatcher('UC1uv2Oq6kNxgATlCiez59hw', 'videos/towa', webhook=webhook)
+YoutubeChannelWatcher('UCa9Y57gfeY0Zro_noHRVrnw', 'videos/luna', webhook=webhook)
