e415051ded0f29b1f50e443a227a08243e4c8815
Jianfeng Zhang <swordfeng123@gmail.com>
2020-06-25 09:09:54 -0700

    main file format

--- main.py
+++ main.py
@@ -7,40 +7,45 @@ webhook = YoutubeWebhook(
     'https://<webhook-url>',
 )
 
-YoutubeChannelWatcher('UCp6993wxpyDPHUpavwDFqgg', 'videos/sora', webhook=webhook)
-YoutubeChannelWatcher('UCDqI2jOz0weumE8s7paEk6g', 'videos/roboco', webhook=webhook)
-YoutubeChannelWatcher('UC-hM6YJuNYVAmUWxeIr9FeA', 'videos/miko', webhook=webhook)
-YoutubeChannelWatcher('UC5CwaMl1eIgY8h02uZw7u8A', 'videos/suisei', webhook=webhook)
+channels = (
+    ('UCp6993wxpyDPHUpavwDFqgg', 'videos/sora'),
+    ('UCDqI2jOz0weumE8s7paEk6g', 'videos/roboco'),
+    ('UC-hM6YJuNYVAmUWxeIr9FeA', 'videos/miko'),
+    ('UC5CwaMl1eIgY8h02uZw7u8A', 'videos/suisei'),
 
-YoutubeChannelWatcher('UC0TXe_LYZ4scaW2XMyi5_kw', 'videos/azki', webhook=webhook)
+    ('UC0TXe_LYZ4scaW2XMyi5_kw', 'videos/azki'),
 
-YoutubeChannelWatcher('UCD8HOxPs4Xvsm8H0ZxXGiBw', 'videos/mel', webhook=webhook)
-YoutubeChannelWatcher('UCQ0UDLQCjY0rmuxCDE38FGg', 'videos/matsuri', webhook=webhook)
-YoutubeChannelWatcher('UC1CfXB_kRs3C-zaeTG3oGyg', 'videos/haato', webhook=webhook)
-YoutubeChannelWatcher('UCHj_mh57PVMXhAUDphUQDFA', 'videos/haato', webhook=webhook)
-YoutubeChannelWatcher('UCFTLzh12_nrtzqBPsTCqenA', 'videos/akirose', webhook=webhook)
-YoutubeChannelWatcher('UCLbtM3JZfRTg8v2KGag-RMw', 'videos/akirose', webhook=webhook)
-YoutubeChannelWatcher('UCdn5BQ06XqgXoAxIhbqw5Rg', 'videos/fubuki', webhook=webhook)
+    ('UCD8HOxPs4Xvsm8H0ZxXGiBw', 'videos/mel'),
+    ('UCQ0UDLQCjY0rmuxCDE38FGg', 'videos/matsuri'),
+    ('UC1CfXB_kRs3C-zaeTG3oGyg', 'videos/haato'),
+    ('UCHj_mh57PVMXhAUDphUQDFA', 'videos/haato'),
+    ('UCFTLzh12_nrtzqBPsTCqenA', 'videos/akirose'),
+    ('UCLbtM3JZfRTg8v2KGag-RMw', 'videos/akirose'),
+    ('UCdn5BQ06XqgXoAxIhbqw5Rg', 'videos/fubuki'),
 
-YoutubeChannelWatcher('UC1opHUrw8rvnsadT-iGp7Cg', 'videos/aqua', webhook=webhook)
-YoutubeChannelWatcher('UCXTpFs_3PqI41qX2d9tL2Rw', 'videos/shion', webhook=webhook)
-YoutubeChannelWatcher('UC7fk0CB07ly8oSl0aqKkqFg', 'videos/ayame', webhook=webhook)
-YoutubeChannelWatcher('UC1suqwovbL1kzsoaZgFZLKg', 'videos/choco', webhook=webhook)
-YoutubeChannelWatcher('UCp3tgHXw_HI0QMk1K8qh3gQ', 'videos/choco', webhook=webhook)
-YoutubeChannelWatcher('UCvzGlP9oQwU--Y0r9id_jnA', 'videos/subaru', webhook=webhook)
+    ('UC1opHUrw8rvnsadT-iGp7Cg', 'videos/aqua'),
+    ('UCXTpFs_3PqI41qX2d9tL2Rw', 'videos/shion'),
+    ('UC7fk0CB07ly8oSl0aqKkqFg', 'videos/ayame'),
+    ('UC1suqwovbL1kzsoaZgFZLKg', 'videos/choco'),
+    ('UCp3tgHXw_HI0QMk1K8qh3gQ', 'videos/choco'),
+    ('UCvzGlP9oQwU--Y0r9id_jnA', 'videos/subaru'),
 
-YoutubeChannelWatcher('UCp-5t9SrOQwXMU7iIjQfARg', 'videos/mio', webhook=webhook)
-YoutubeChannelWatcher('UCvaTdHTWBGv3MKj3KVqJVCw', 'videos/okayu', webhook=webhook)
-YoutubeChannelWatcher('UChAnqc_AY5_I3Px5dig3X1Q', 'videos/korone', webhook=webhook)
+    ('UCp-5t9SrOQwXMU7iIjQfARg', 'videos/mio'),
+    ('UCvaTdHTWBGv3MKj3KVqJVCw', 'videos/okayu'),
+    ('UChAnqc_AY5_I3Px5dig3X1Q', 'videos/korone'),
 
-YoutubeChannelWatcher('UC1DCedRgGHBdm81E1llLhOQ', 'videos/pekora', webhook=webhook)
-YoutubeChannelWatcher('UCl_gCybOJRIgOXw6Qb4qJzQ', 'videos/rushia', webhook=webhook)
-YoutubeChannelWatcher('UCvInZx9h3jC2JzsIzoOebWg', 'videos/flare', webhook=webhook)
-YoutubeChannelWatcher('UCdyqAaZDKHXg4Ahi7VENThQ', 'videos/noel', webhook=webhook)
-YoutubeChannelWatcher('UCCzUftO8KOVkV4wQG1vkUvg', 'videos/marine', webhook=webhook)
+    ('UC1DCedRgGHBdm81E1llLhOQ', 'videos/pekora'),
+    ('UCl_gCybOJRIgOXw6Qb4qJzQ', 'videos/rushia'),
+    ('UCvInZx9h3jC2JzsIzoOebWg', 'videos/flare'),
+    ('UCdyqAaZDKHXg4Ahi7VENThQ', 'videos/noel'),
+    ('UCCzUftO8KOVkV4wQG1vkUvg', 'videos/marine'),
 
-YoutubeChannelWatcher('UCZlDXzGoo7d44bwdNObFacg', 'videos/kanata', webhook=webhook)
-YoutubeChannelWatcher('UCS9uQI-jC3DE0L4IpXyvr6w', 'videos/coco', webhook=webhook)
-YoutubeChannelWatcher('UCqm3BQLlJfvkTsX_hvm0UmA', 'videos/watame', webhook=webhook)
-YoutubeChannelWatcher('UC1uv2Oq6kNxgATlCiez59hw', 'videos/towa', webhook=webhook)
-YoutubeChannelWatcher('UCa9Y57gfeY0Zro_noHRVrnw', 'videos/luna', webhook=webhook)
+    ('UCZlDXzGoo7d44bwdNObFacg', 'videos/kanata'),
+    ('UCS9uQI-jC3DE0L4IpXyvr6w', 'videos/coco'),
+    ('UCqm3BQLlJfvkTsX_hvm0UmA', 'videos/watame'),
+    ('UC1uv2Oq6kNxgATlCiez59hw', 'videos/towa'),
+    ('UCa9Y57gfeY0Zro_noHRVrnw', 'videos/luna'),
+)
+
+for channel_id, path in channels:
+    YoutubeChannelWatcher(channel_id, path, webhook=webhook)
